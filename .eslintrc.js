0dbfabe773ad0af0b1c87834408a83a9f37bd8f3
Kas <kazaakas@hotmail.com>
2020-03-08 15:50:26 +0100

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,35 @@ const path = require('path')
 
 /* User defined constants */
 
-/* Environment: */
 const ecmaVersion = 11
 const minSupportedEcmaVersion = 10
 const browser = false
 const imports = true
 const node = true
 const babel = false
+const typescript = true
 
-/* Frameworks and libraries: */
 const react = false
 const jquery = false
 const jest = false
 
-/* Styling: */
 const checkStyling = false
 const indentSetting = 'tab'
+const semiSetting = 'always'
 
 /* End of user defined constants */
 
-const reactVersion = react && require('react').version // eslint-disable-line global-require
-let importPath
+let configFile
 try {
-  importPath = require('./jsconfig.json').compilerOptions.baseUrl
-} catch {
-  importPath = '.'
+  configFile = require(typescript ? './tsconfig.json' : './jsconfig.json')
+} catch (e) {
+  if (typescript) throw e
+  configFile = {}
 }
+const { strict, strictNullChecks } = configFile.compilerOptions || {}
+
+const reactVersion = react && require('react').version // eslint-disable-line global-require
+const tsPrefix = typescript ? '@typescript-eslint/' : ''
 
 const env = process.env.NODE_ENV || ''
 const isInProductionMode = env.includes('production')
@@ -50,11 +53,13 @@ module.exports = {
     'eslint:recommended',
     jest && 'plugin:jest/recommended',
     react && 'plugin:react/recommended',
+    typescript && 'plugin:@typescript-eslint/recommended',
   ].filter(Boolean),
-  parser: babel ? 'babel-eslint' : null,
+  parser: babel ? 'babel-eslint' : typescript ? '@typescript-eslint/parser' : null,
   parserOptions: {
     sourceType: 'module',
     ecmaVersion,
+    project: typescript ? './tsconfig.json' : null,
     ecmaFeatures: {
       impliedStrict: true,
       jsx: react,
@@ -63,6 +68,7 @@ module.exports = {
   plugins: [
     jest && 'jest',
     react && ['react', 'react-hooks'],
+    typescript && '@typescript-eslint',
     imports && 'import',
     node && 'node',
     ecmaVersion >= 6 && 'promise',
@@ -81,7 +87,7 @@ module.exports = {
       'arrow-spacing': style,
       'block-scoped-var': 'error',
       'block-spacing': style,
-      'brace-style': [style, '1tbs', { allowSingleLine: true }],
+      [`${tsPrefix}brace-style`]: [style, '1tbs', { allowSingleLine: true }],
       'callback-return': [
         'warn',
         [
@@ -117,10 +123,11 @@ module.exports = {
           objects: 'always-multiline',
           imports: 'always-multiline',
           exports: 'always-multiline',
-          functions: minSupportedEcmaVersion >= 6 ? 'always-multiline' : 'ignore',
+          functions:
+            babel || typescript || minSupportedEcmaVersion >= 6 ? 'always-multiline' : 'ignore',
         },
       ],
-      'comma-spacing': style,
+      [`${tsPrefix}comma-spacing`]: style,
       'comma-style': style,
       complexity: 'off',
       'computed-property-spacing': [style, 'never'],
@@ -129,26 +136,30 @@ module.exports = {
       'constructor-super': 'error',
       curly: [style, 'multi-line'],
       'default-case': 'warn',
+      'default-case-last': 'error',
+      [`${tsPrefix}default-param-last`]: 'error',
       'dot-location': [style, 'property'],
       'dot-notation': 'warn', // is "suggestion" -> annoying
       'eol-last': [style, 'never'],
       eqeqeq: 'warn', // is "suggestion" -> not func eq
       'for-direction': 'off',
-      'func-call-spacing': style,
+      [`${tsPrefix}func-call-spacing`]: style,
       'func-name-matching': 'off',
       'func-names': 'off',
       'func-style': 'off',
+      'function-call-argument-newline': 'off',
       'function-paren-newline': [style, 'consistent'],
       'generator-star-spacing': style,
       'getter-return': 'error',
       'global-require': 'warn',
+      'grouped-accessor-pairs': 'error',
       'guard-for-in': 'off',
       'handle-callback-err': 'warn',
       'id-blacklist': 'off',
       'id-length': 'off',
       'id-match': 'off',
       'implicit-arrow-linebreak': [style, 'beside'],
-      indent: [
+      [`${tsPrefix}indent`]: [
         style,
         indentSetting,
         {
@@ -186,7 +197,7 @@ module.exports = {
       'new-parens': 'warn',
       'newline-per-chained-call': [style, { ignoreChainWithDepth: 3 }],
       'no-alert': 'warn',
-      'no-array-constructor': 'warn',
+      [`${tsPrefix}no-array-constructor`]: 'warn',
       'no-async-promise-executor': 'error',
       'no-await-in-loop': 'off',
       'no-bitwise': 'off',
@@ -201,19 +212,21 @@ module.exports = {
       'no-console': production_warn,
       'no-const-assign': 'error',
       'no-constant-condition': 'error',
+      'no-constructor-return': 'error',
       'no-control-regex': 'warn',
       'no-delete-var': 'error',
       'no-debugger': production_error,
       'no-div-regex': 'off',
       'no-dupe-args': 'error',
-      'no-dupe-class-members': 'error',
+      [`${tsPrefix}no-dupe-class-members`]: 'error',
+      'no-dupe-else-if': 'error',
       'no-dupe-keys': 'error',
       'no-duplicate-case': 'error',
       'no-duplicate-imports': 'off', // disabled in favor of import/no-duplicates
       'no-else-return': 'warn',
       'no-empty': production_error, // should have justifying comment
       'no-empty-character-class': 'warn',
-      'no-empty-function': 'off',
+      [`${tsPrefix}no-empty-function`]: 'off',
       'no-empty-pattern': 'warn',
       'no-eq-null': 'off',
       'no-eval': 'error',
@@ -222,15 +235,16 @@ module.exports = {
       'no-extra-boolean-cast': 'warn',
       'no-extra-bind': 'warn', // is "suggestion" -> not func eq
       'no-extra-label': 'off', // is "suggestion" -> annoying  // no-labels
-      'no-extra-parens': 'off', // useful for unambiguity
-      'no-extra-semi': 'off',
+      [`${tsPrefix}no-extra-parens`]: 'off', // useful for unambiguity
+      [`${tsPrefix}no-extra-semi`]: style,
+      'no-import-assign': 'error',
       'no-fallthrough': 'warn',
       'no-floating-decimal': style,
       'no-func-assign': 'error',
       'no-global-assign': 'error',
       'no-implicit-coercion': 'warn',
       'no-implicit-globals': 'error',
-      'no-implied-eval': 'error',
+      [`${tsPrefix}no-implied-eval`]: 'error',
       'no-inline-comments': 'off',
       'no-inner-declarations': 'off',
       'no-invalid-regexp': 'error',
@@ -242,7 +256,7 @@ module.exports = {
       'no-lone-blocks': 'warn',
       'no-lonely-if': 'warn',
       'no-loop-func': 'warn',
-      'no-magic-numbers': 'off',
+      [`${tsPrefix}no-magic-numbers`]: 'off',
       'no-misleading-character-class': 'warn',
       'no-mixed-operators': 'off',
       'no-mixed-requires': 'warn',
@@ -272,15 +286,17 @@ module.exports = {
       'no-redeclare': 'error',
       'no-regex-spaces': 'warn',
       'no-restricted-globals': 'error',
+      'no-restricted-exports': 'error',
       'no-restricted-imports': 'error',
       'no-restricted-modules': 'error',
       'no-restricted-properties': 'error',
       'no-restricted-syntax': 'error',
       'no-return-assign': 'error',
-      'no-return-await': 'warn',
+      [`${tsPrefix}no-return-await`]: 'warn',
       'no-script-url': 'warn',
       'no-self-assign': 'warn',
       'no-self-compare': 'warn',
+      'no-setter-return': 'error',
       'no-sequences': 'error',
       'no-shadow': 'warn',
       'no-shadow-restricted-names': 'error',
@@ -290,7 +306,7 @@ module.exports = {
       'no-template-curly-in-string': 'warn',
       'no-ternary': 'off',
       'no-this-before-super': 'warn',
-      'no-throw-literal': 'off', // disabled in favor of async fns
+      [`${tsPrefix}no-throw-literal`]: 'off', // disabled in favor of async fns
       'no-trailing-spaces': style,
       'no-undef-init': 'warn',
       'no-undef': 'error',
@@ -301,24 +317,25 @@ module.exports = {
       'no-unreachable': 'warn',
       'no-unsafe-finally': 'warn',
       'no-unsafe-negation': 'warn',
-      'no-unused-expressions': 'off',
+      [`${tsPrefix}no-unused-expressions`]: 'off',
       'no-unused-labels': 'off', // is "suggestion" -> annoying  // no-labels
       'no-unexpected-multiline': 'error',
-      'no-unused-vars': [
+      [`${tsPrefix}no-unused-vars`]: [
         'warn',
         {
           ignoreRestSiblings: true,
         },
       ],
-      'no-use-before-define': [
+      [`${tsPrefix}no-use-before-define`]: [
         'error',
         {
           functions: false,
         },
       ],
+      'no-useless-backreference': 'warn',
       'no-useless-catch': 'warn',
       'no-useless-computed-key': 'warn',
-      'no-useless-constructor': 'warn',
+      [`${tsPrefix}no-useless-constructor`]: 'warn',
       'no-useless-call': 'warn',
       'no-useless-rename': 'warn',
       'no-useless-concat': 'warn',
@@ -340,7 +357,7 @@ module.exports = {
       ],
       'object-curly-spacing': [style, 'always'],
       'object-property-newline': [style, { allowAllPropertiesOnSameLine: true }],
-      'object-shorthand': babel || minSupportedEcmaVersion > 5 ? 'warn' : 'off',
+      'object-shorthand': babel || typescript || minSupportedEcmaVersion >= 6 ? 'warn' : 'off',
       'one-var': ['error', 'never'],
       'one-var-declaration-per-line': 'off',
       'operator-assignment': 'off',
@@ -348,31 +365,34 @@ module.exports = {
       'padded-blocks': 'off',
       'padding-line-between-statements': 'error',
       'prefer-arrow-callback': 'off',
-      'prefer-const': 'warn', // is "suggestion" -> annoying
+      'prefer-const': 'warn',
       'prefer-destructuring': 'off',
+      'prefer-exponentiation-operator':
+        babel || typescript || minSupportedEcmaVersion >= 7 ? 'error' : 'off',
       'prefer-named-capture-group': 'off',
       'prefer-numeric-literals': 'off',
       'prefer-object-spread': 'off',
       'prefer-promise-reject-errors': 'off',
+      'prefer-regex-literals': 'off',
       'prefer-rest-params': 'off',
       'prefer-spread': 'off',
       'prefer-template': 'off',
       'quote-props': [style, 'consistent-as-needed'],
-      quotes: [style, 'single', { avoidEscape: true }],
+      [`${tsPrefix}quotes`]: [style, 'single', { avoidEscape: true }],
       radix: 'off',
       'require-atomic-updates': 'warn',
-      'require-await': 'warn',
+      [`${tsPrefix}require-await`]: 'off',
       'require-unicode-regexp': 'off',
       'require-yield': 'warn',
       'rest-spread-spacing': style,
-      semi: [style, 'always'],
+      [`${tsPrefix}semi`]: [style, semiSetting],
       'semi-spacing': style,
       'semi-style': style,
       'sort-imports': 'off',
       'sort-keys': 'off',
       'sort-vars': 'off',
       'space-before-blocks': style,
-      'space-before-function-paren': [
+      [`${tsPrefix}space-before-function-paren`]: [
         style,
         {
           anonymous: 'never',
@@ -641,11 +661,87 @@ module.exports = {
       'promise/prefer-await-to-then': 'off',
       'promise/prefer-await-to-callbacks': 'off',
     },
+    typescript && {
+      '@typescript-eslint/adjacent-overload-signatures': 'warn',
+      '@typescript-eslint/array-type': 'warn',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/ban-types': 'error',
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' },
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-member-accessibility': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/member-delimiter-style': [
+        style,
+        {
+          multiline: { delimiter: semiSetting ? 'semi' : 'none', requireLast: true },
+          singleline: { delimiter: 'comma', requireLast: false },
+        },
+      ],
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-base-to-string': 'error',
+      '@typescript-eslint/no-dynamic-delete': 'off',
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-explicit-any': strict ? 'error' : 'off',
+      '@typescript-eslint/no-extra-non-null-assertion': 'error',
+      '@typescript-eslint/no-extraneous-class': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-for-in-array': 'warn',
+      '@typescript-eslint/no-inferrable-types': 'warn',
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+      '@typescript-eslint/no-non-null-assertion': strict || strictNullChecks ? 'error' : 'off',
+      '@typescript-eslint/no-parameter-properties': 'off',
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-this-alias': 'error',
+      '@typescript-eslint/no-type-alias': 'off',
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+      '@typescript-eslint/no-unnecessary-condition': 'errpr',
+      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+      '@typescript-eslint/no-unused-vars-experimental': 'off',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/prefer-as-const': 'error',
+      '@typescript-eslint/prefer-for-of': 'warn',
+      '@typescript-eslint/prefer-function-type': 'off',
+      '@typescript-eslint/prefer-includes': 'warn',
+      '@typescript-eslint/prefer-namespace-keyword': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+      '@typescript-eslint/prefer-optional-chain': 'warn',
+      '@typescript-eslint/prefer-readonly': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/prefer-regexp-exec': 'warn',
+      '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+      '@typescript-eslint/promise-function-async': 'warn',
+      '@typescript-eslint/require-array-sort-compare': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/switch-exhaustiveness-check': 'off',
+      '@typescript-eslint/triple-slash-reference': 'error',
+      '@typescript-eslint/type-annotation-spacing': style,
+      '@typescript-eslint/typedef': 'off',
+      '@typescript-eslint/unbound-method': 'error',
+      '@typescript-eslint/unified-signatures': 'warn',
+    },
   ),
   settings: {
     'import/resolver': {
       node: {
-        paths: [path.resolve(__dirname, importPath)],
+        paths: [
+          path.resolve(
+            __dirname,
+            (configFile.compilerOptions && configFile.compilerOptions.baseUrl) || '.',
+          ),
+        ],
         extensions: ['.js', '.jsx'],
       },
     },
@@ -656,8 +752,16 @@ module.exports = {
   },
 }
 
-/* To get all rules (to check for updates/deprecations), goto: https://eslint.org/docs/rules/
- * Then: [...document.querySelectorAll("table.rule-list td:nth-child(3) p a")].map(x => x.innerHTML)
+/* To get all rules (to check for updates/deprecations):
+ * Goto: https://eslint.org/docs/rules/ and run:
+ * const pageRules = [...document.querySelectorAll("table.rule-list td:nth-child(3) p a")].map(x => x.innerHTML)
+ *
+ * Run the following bash command and paste the contents into a const myRules
+ * node -e "console.log(require('./.eslintrc.js').rules)"
+ *
+ * Then you can run these checks:
+ * pageRules.filter(x => !myRules.includes(x))
+ * myRules.filter(x => !pageRules.includes(x) && !x.includes('/'))
+ * myRules.filter(x => myRules.includes(`@typescript-eslint/${x}`))
  */
-
 console.log('eslint config parsed')
